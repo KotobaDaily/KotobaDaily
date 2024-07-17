198111b537dd9fe4f7acf0ca1f61c99795ab0fae
Brandon Shippy <0brandonshippy@gmail.com>
2024-07-18 03:39:05 +0900

    Edited API calls and view for Yesterday functionality

--- KotobaDaily/APITests.swift
+++ KotobaDaily/APITests.swift
@@ -62,3 +62,56 @@ class WOTDViewModel: ObservableObject {
         cancellable?.cancel() // Prevents memory leak
     }
 }
+
+class yesterdayWOTDViewModel: ObservableObject {
+    @Published var words: [WOTD] = [] // Allows updates to be made and automatically trigger them uses our struct Word
+    private var cancellable: AnyCancellable? // Allows the network request to be cancelled
+    
+    @Published var wordoftheday = "寝不足" // Define variables to hold the fetched information from. @Published makes it update.
+    @Published var definition = "lack of sleep; insufficient sleep"
+    @Published var example = "寝不足の影響がではじめた。"
+    @Published var furigana = "ねぶそく"
+    
+    private var hasFetched = false // Makes it so it only checks ONCE. Bandaid fix for now, requires user to restart
+    
+    func fetchWords() { // function for getting information from the URL
+        guard !hasFetched else { return }
+                hasFetched = true
+        
+        guard let url = URL(string: "http://localhost:8000/yesterday/") else { // let keyword is a const
+            print("Invalid URL")
+            return
+        }
+        
+        cancellable = URLSession.shared.dataTaskPublisher(for: url) // since we define it as cancellable we can cancel if needed
+            .map { $0.data } // Array of JSON Data
+            .decode(type: [WOTD].self, decoder: JSONDecoder()) // WOTD being our struct
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error fetching words: \(error)")
+                }
+            }, receiveValue: { response in // Filling in our struct
+                self.words = response
+                if let firstWord = response.first { // Get the whole struct. Then assign each part of the struc to our @Publisher varaibles, which will update the user.
+                    self.wordoftheday = firstWord.Word
+                    self.definition = firstWord.Definition
+                    self.example = firstWord.Example
+                    self.furigana = firstWord.Furigana
+                }
+                
+            })
+    }
+    
+    func refreshWords() { // USE THIS WHEN WANTING TO LET THEM MAKE ANOTHER API CALL
+            hasFetched = false
+            fetchWords()
+        }
+    
+    deinit {
+        cancellable?.cancel() // Prevents memory leak
+    }
+}
--- KotobaDaily/YesterdayView.swift
+++ KotobaDaily/YesterdayView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct YesterdayView: View {
+    @StateObject private var pullWords = yesterdayWOTDViewModel()
+    
     var body: some View {
-        Text("Yesterday View Placeholder")
+        VStack {
+            Text(pullWords.wordoftheday)
+                .font(.wotdText)
+                .padding(.horizontal, 16.0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("("+pullWords.furigana+")")
+                .font(.furigana)
+                .padding(.horizontal, 16.0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("Definition: " + pullWords.definition)
+                .font(.generalTextSmall)
+                .padding([.leading, .bottom, .trailing], 16.0)
+                .padding(.top, 4.0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("Example: " + pullWords.example)
+                .font(.generalTextSmall)
+                .padding(.all, 16.0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+        }
+                .onAppear{
+                    pullWords.fetchWords()
+                }
     }
 }
 
